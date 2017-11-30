a75154ad1418324622b37f354d6363c8ffb6ed15
Niko Lagman <Lagman.Niko@gmail.com>
2017-11-29 22:19:55 -0500

    Add sql query response to api.php

--- api.php
+++ api.php
@@ -1,12 +1,20 @@
 <?php
+  header('Content-type: application/json');
   $servername = "localhost";
   $username = "lagman";
   $password = "niko4850";
   $database = 'pageVisits';
-  $port = 3306;
   $conn = new mysqli($servername, $username, $password, $database, $port);
   if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
-  echo "Connected successfully";
+
+  $sql = "SELECT * FROM visitInfo";
+  $result = $conn->query($sql);
+  $rows;
+  while ($row = $result->fetch_assoc()) {
+    $rows[] = $row;
+  }
+  echo json_encode($rows);
+
 ?>
